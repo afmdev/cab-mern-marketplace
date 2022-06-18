7c67dcb6816946fe2174b2b4f79ad0773220c5e2
Alejandro Marrero <crixto@gmail.com>
2022-06-18 16:23:56 +0200

    Creating new views with Material UI

---
+++ client/src/views/ViewHome.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import TopBar from '../components/TopBar'
+import SearchBar from '../components/SearchBar'
+import MainMenu from '../components/TopSlider'
+
+function ViewHome() {
+	return (
+		<>
+			<TopBar />
+			<SearchBar />
+			<MainMenu />
+		</>
+
+	)
+}
+
+export default ViewHome
