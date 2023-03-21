d3cbb2efc54558d08ad2cc95030443539140560a
afm: alejandrofm.com <crixto@gmail.com>
2023-03-21 17:11:11 +0100

    Setting up a new server url in config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const serverURL =
 	process.env.NODE_ENV === "development"
 		? "http://localhost:5000/"
-		: "https://mern-server-eta.vercel.app/";
+		: "https://mern-server-azure.vercel.app/";
 
 export default serverURL;
